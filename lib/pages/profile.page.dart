e868f3cd5d086517089e811eac1404ed0e73dc48
jessicapcruz <jessica_p_cruz@hotmail.com>
2022-03-14 23:45:27 -0300

    informações de perfil

--- lib/pages/profile.page.dart
+++ lib/pages/profile.page.dart
@@ -19,6 +19,7 @@ class _ProfilePageState extends State<ProfilePage>  {
   @override
   Widget build(BuildContext context) {
     setState(() {
+      followers = [];
       //this function gets a user from the username supplied in the input
       //  user = Provider.of<UserProvider>(context).getUser();
 
@@ -32,112 +33,164 @@ class _ProfilePageState extends State<ProfilePage>  {
       });
     });
 
-
     return Scaffold(
       body: Container(
         padding: EdgeInsets.only(top: 10, left: 40, right: 40),
         color: Colors.white,
-        child: ListView(
-          children: <Widget>[
-            Container(
-              width: 200,
-              height: 200,
-              alignment: Alignment(0.0, 1.15),
-              decoration: new BoxDecoration(
-                image: new DecorationImage(
-                  image: AssetImage("profile-picture.png"),
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              child: Container(
-                height: 56,
-                width: 56,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    stops: [0.3, 1.0],
-                    colors: [
-                      Color(0xFF245FF5),
-                      Color(0XFFF92B7F),
-                    ],
-                  ),
-                  border: Border.all(
-                    width: 4.0,
-                    color: const Color(0xFFFFFFFF),
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(56),
-                  ),
-                ),
-                child: SizedBox.expand(
-                  child: FlatButton(
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
+        child: followers != null
+        ?
+    ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: followers.length, // the number of followers a user has
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: Colors.grey)),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    width: 50,
+                    child: CircleAvatar(
+                      // followers's profile picture
+                      backgroundImage: NetworkImage("profile-picture.png"),
                     ),
-                    onPressed: () {},
                   ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.account_circle_outlined),
-                    title: Text('Dados Pessoais'),
+                  SizedBox(
+                    width: 20,
                   ),
-                  const ListTile(
-                    subtitle: Text('Nome: '),
+                  Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const ListTile(
+                          leading: Icon(Icons.account_circle_outlined),
+                          title: Text('Dados Pessoais'),
+                        ),
+                        const ListTile(
+                          subtitle: Text('Nome: '),
+                        ),
+                        followers[index].nome_completo != null
+                            ? Text(
+                          followers[index].nome_completo,
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+                            : Text(
+                          "Not Available",
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        ),
+
+                        const ListTile(
+                          subtitle: Text('CPF: '),
+                        ),
+                        followers[index].cpf != null
+                            ? Text(
+                          followers[index].cpf,
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+                            : Text(
+                          "Not Available",
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        ),
+                        const ListTile(
+                          subtitle: Text('Nome da mãe: '),
+                        ),
+                        followers[index].nome_mae != null
+                            ? Text(
+                          followers[index].nome_mae,
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+                            : Text(
+                          "Not Available",
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+
+                      ],
+                    ),
                   ),
-                  const ListTile(
-                    subtitle: Text('Data de nascimento: '),
-                  ),
-                  const ListTile(
-                    subtitle: Text('CPF: '),
+                  Card(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const ListTile(
+                          subtitle: Text('Endereço: '),
+                        ),
+                        followers[index].endereco != null
+                            ? Text(
+                          followers[index].endereco,
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+                            : Text(
+                          "Not Available",
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        ),
+                        const ListTile(
+                          subtitle: Text('Telefone: '),
+                        ),
+                        followers[index].telefone != null
+                            ? Text(
+                          followers[index].telefone,
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+                            : Text(
+                          "Not Available",
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        ),
+                        const ListTile(
+                          subtitle: Text('E-mail: '),
+                        ),
+                        followers[index].email != null
+                            ? Text(
+                          followers[index].email,
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        )
+                            : Text(
+                          "Not Available",
+                          style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w700),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Ink(
+                              decoration: const ShapeDecoration(
+                                color: Colors.lightBlue,
+                                shape: CircleBorder(),
+                              ),
+                              child: IconButton(
+                                icon: const Icon(Icons.create_outlined),
+                                color: Colors.white,
+                                onPressed: () {},
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
-            Card(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  const ListTile(
-                    leading: Icon(Icons.album),
-                    title: Text('Endereço:'),
-                    subtitle: Text('Cidade:'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      Ink(
-                        decoration: const ShapeDecoration(
-                          color: Colors.lightBlue,
-                          shape: CircleBorder(),
-                        ),
-                        child: IconButton(
-                          icon: const Icon(Icons.create_outlined),
-                          color: Colors.white,
-                          onPressed: () {},
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        );
+      },
+    )
+        :  Container(
+            child: Align(
+                child: Text(
+                    'Not Available')
+              )
+            ), // before the data is returned, indicate loading data
       ),
     );
   }
+
 
   @override
   State<StatefulWidget> createState() {
